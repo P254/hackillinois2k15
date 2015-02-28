0b73b6e2023c4103010132201407dde533565517
Pranav Chaudhari <awesomesauceproduction@gmail.com>
2015-02-28 15:32:35 -0500

    added location rom js

---
+++ src/pebble-js-app.js
@@ -0,0 +1,20 @@
+var locationOptions = {
+  enableHighAccuracy: true, 
+  maximumAge: 10000, 
+  timeout: 10000
+};
+
+function locationSuccess(pos) {
+  console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
+}
+
+function locationError(err) {
+  console.log('location error (' + err.code + '): ' + err.message);
+}
+
+Pebble.addEventListener('ready',
+  function(e) {
+    // Request current position
+    navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+  }
+);
